87f8b2d84f21aed4247b34192afd3a88cf056dda
alial95 <alistair_j_cooper@hotmail.com>
2020-10-31 12:48:09 +0000

    fgfgf

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Log_Project')],
+        'DIRS': [os.path.join(BASE_DIR, 'learning_logs/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
